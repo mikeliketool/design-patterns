4b2b51828e584fae8b87faa0dcb1f916bd32ba89
Mike Fehrenbach <fehrenbach.mike@gmail.com>
2021-02-13 22:43:59 -0500

    Revert "Switch component inclusion to main.js"
    This reverts commit fb25a11630107ff7c45959ec73e9ce579ef38b5e.

--- src/solid/interface_segregation_principle.py
+++
@@ -1,3 +0,0 @@
-# no client should be forced to depend on methods it does not use.
-# ISP splits interfaces that are very large into smaller and more specific ones so that clients will only have to know
-# about the methods that are of interest to them
