f7347d000e352b6b09459035dc504a80b924b653
Mike Fehrenbach <fehrenbach.mike@gmail.com>
2021-03-01 11:23:57 -0500

    Example of the interface segregation principle

---
+++ src/solid/integration_segregation_principle.py
@@ -0,0 +1,86 @@
+# Clients should not be forced to depend upon interfaces that they do not use.
+
+from abc import abstractmethod
+
+
+# broken
+class Machine:
+    def print(self, document):
+        raise NotImplementedError
+
+    def fax(self, document):
+        raise NotImplementedError
+
+    def scan(self, document):
+        raise NotImplementedError
+
+
+class MultiFunctionPrinter(Machine):
+    def print(self, document):
+        pass
+
+    def fax(self, document):
+        pass
+
+    def scan(self, document):
+        pass
+
+
+class OldFashionedPrinter(Machine):
+    def print(self, document):
+        # ok
+        pass
+
+    def fax(self, document):
+        pass  # noop
+
+    def scan(self, document):
+        raise NotImplementedError('Printer cannot scan!')
+
+
+# working
+class Printer:
+    @abstractmethod
+    def print(self, document):
+        pass
+
+
+class Scanner:
+    @abstractmethod
+    def scan(self, document):
+        pass
+
+
+class MyPrinter(Printer):
+    def print(self, document):
+        print(document)
+
+
+class Photocopier(Printer, Scanner):
+    def print(self, document):
+        print(document)
+
+    def scan(self, document):
+        pass
+
+
+class MultiFunctionDevice(Printer, Scanner):
+    @abstractmethod
+    def print(self, document):
+        pass
+
+    @abstractmethod
+    def scan(self, document):
+        pass
+
+
+class MultiFunctionMachine(MultiFunctionDevice):
+    def __init__(self, printer, scanner):
+        self.printer = printer
+        self.scanner = scanner
+
+    def print(self, document):
+        self.printer.print(document)
+
+    def scan(self, document):
+        self.scanner.scan(document)
